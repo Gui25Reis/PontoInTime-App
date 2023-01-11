5df3864948b18eccf83492ca92ae0b4d76882798
Gui Reis <gui.sreis25@gmail.com>
2023-01-10 23:20:29 -0300

    Arrumar pasta de extensions

--- Ponto In Time/iOS-App/General/Extensions/UIViewController/UIViewController+showWarningPopUp.swift
+++ Ponto In Time/iOS-App/General/Extensions/UIViewController/UIViewController+showWarningPopUp.swift
@@ -9,9 +9,7 @@ extension UIViewController {
     /// Mostra o pop up em casos de erro
     /// - Parameter error: erro pra ser mostrado
     public func showWarningPopUp(with error: ErrorWarnings?) {
-        guard let error else { return }
-        
-        let alert = error.getWarningAlert()
-        self.present(alert, animated: true)
+        guard let alert = error?.getWarningAlert() else { return }
+        self.showAlert(alert)
     }
 }
